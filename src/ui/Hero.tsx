51d10665db0f2ecd71e31cbf493befee2c3370b3
Kevin G <ge.kevin.a@gmail.com>
2021-07-09 22:08:57 -0500

    Create Hero component to keep mess in one file

---
+++ src/ui/Hero.tsx
@@ -0,0 +1,101 @@
+import { FC } from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Theme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  bgSlant1_1440up: {
+    width: "100vw",
+    [theme.breakpoints.up("1440")]: {
+      display: "initial",
+    },
+    [theme.breakpoints.down("1440")]: {
+      display: "none",
+    },
+  },
+  bgSlant1_1200up: {
+    width: "100vw",
+    [theme.breakpoints.up("1440")]: {
+      display: "none !important",
+    },
+    [theme.breakpoints.up("1200")]: {
+      display: "initial",
+    },
+    [theme.breakpoints.down("1200")]: {
+      display: "none",
+    },
+  },
+  bgSlant1_1000up: {
+    width: "100vw",
+    [theme.breakpoints.up("1200")]: {
+      display: "none !important",
+    },
+    [theme.breakpoints.up("1000")]: {
+      display: "initial",
+    },
+    [theme.breakpoints.down("1000")]: {
+      display: "none",
+    },
+  },
+  bgSlant1_800up: {
+    width: "100vw",
+    [theme.breakpoints.up("1000")]: {
+      display: "none !important",
+    },
+    [theme.breakpoints.up("800")]: {
+      display: "initial",
+    },
+    [theme.breakpoints.down("800")]: {
+      display: "none",
+    },
+  },
+  bgSlant1_0up: {
+    width: "100vw",
+    marginTop: "-1px",
+    minWidth: "800px",
+    [theme.breakpoints.up("800")]: {
+      display: "none",
+    },
+    [theme.breakpoints.down("800")]: {
+      display: "initial",
+    },
+  },
+  bgSlant1_test: {
+    width: "100vw",
+    height: "70vh",
+    marginTop: "-1px",
+    backgroundImage: "url('/svgs/bg_slant_1_black_0up.svg')",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  },
+}));
+
+export const Hero: FC = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <img
+        className={classes.bgSlant1_1440up}
+        src="/svgs/bg_slant_1_black_1440up.svg"
+        alt=""
+      />
+      <img
+        className={classes.bgSlant1_1200up}
+        src="/svgs/bg_slant_1_black_1200up.svg"
+        alt=""
+      />
+      <img
+        className={classes.bgSlant1_1000up}
+        src="/svgs/bg_slant_1_black_1000up.svg"
+        alt=""
+      />
+      <img
+        className={classes.bgSlant1_800up}
+        src="/svgs/bg_slant_1_black_800up.svg"
+        alt=""
+      />
+      {/* mobile needs less fluid image */}
+      <div className={classes.bgSlant1_test}></div>
+    </>
+  );
+};
