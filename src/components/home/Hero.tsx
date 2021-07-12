c5bec93292d75ec98ee8492b2a29d2103103d6a1
Kevin G <ge.kevin.a@gmail.com>
2021-07-12 17:41:39 -0500

    Fixed incorrect paths to slant_1 svgs

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -52,15 +52,9 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   bgSlant1_0up: {
     width: "100vw",
-    marginTop: "-1px",
-    minWidth: "800px",
-    display: "",
-  },
-  bgSlant1_test: {
-    width: "100vw",
     height: "70vh",
     marginTop: "-1px",
-    backgroundImage: "url('/svgs/bg_slant_1_black_0up.svg')",
+    backgroundImage: "url('/svgs/slant_1/bg_slant_1_black_0up.svg')",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     [theme.breakpoints.up("800")]: {
@@ -143,26 +137,26 @@ export const Hero: FC = () => {
       </div>
       <img
         className={classes.bgSlant1_1440up}
-        src="/svgs/bg_slant_1_black_1440up.svg"
+        src="/svgs/slant_1/bg_slant_1_black_1440up.svg"
         alt=""
       />
       <img
         className={classes.bgSlant1_1200up}
-        src="/svgs/bg_slant_1_black_1200up.svg"
+        src="/svgs/slant_1/bg_slant_1_black_1200up.svg"
         alt=""
       />
       <img
         className={classes.bgSlant1_1000up}
-        src="/svgs/bg_slant_1_black_1000up.svg"
+        src="/svgs/slant_1/bg_slant_1_black_1000up.svg"
         alt=""
       />
       <img
         className={classes.bgSlant1_800up}
-        src="/svgs/bg_slant_1_black_800up.svg"
+        src="/svgs/slant_1/bg_slant_1_black_800up.svg"
         alt=""
       />
       {/* mobile needs less fluid image */}
-      <div className={classes.bgSlant1_test}></div>
+      <div className={classes.bgSlant1_0up}></div>
     </div>
   );
 };
